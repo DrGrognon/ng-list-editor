6443ee2a6e086891a68954c6adb7917fbab40f29
Benjamin GONON <bgonon@homiwoo.com>
2026-02-17 12:00:46 +0100

    Remove useless imports

--- src/app/list-editor/list-editor.ts
+++ src/app/list-editor/list-editor.ts
@@ -9,13 +9,9 @@ import {
   Signal,
   TemplateRef
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
 
 
 export interface EditableItem<T> {
